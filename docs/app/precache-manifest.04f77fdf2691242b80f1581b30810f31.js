62ab910422180b3133b0a3295d8554ea04d3d119
pcroppi <pcroppi@gmail.com>
2020-12-15 20:10:38 +0100

    react

---
+++ docs/app/precache-manifest.04f77fdf2691242b80f1581b30810f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9876752fb5c80cf5067463ae4482ace1",
+    "url": "/react-mablor/index.html"
+  },
+  {
+    "revision": "477b752829cc6586cca4",
+    "url": "/react-mablor/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3c818fac1ac8fa3abd7a",
+    "url": "/react-mablor/static/css/main.49c80ad3.chunk.css"
+  },
+  {
+    "revision": "477b752829cc6586cca4",
+    "url": "/react-mablor/static/js/2.3821f2ae.chunk.js"
+  },
+  {
+    "revision": "34717af1a52cd36d6c2f928711f90359",
+    "url": "/react-mablor/static/js/2.3821f2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c818fac1ac8fa3abd7a",
+    "url": "/react-mablor/static/js/main.2db47733.chunk.js"
+  },
+  {
+    "revision": "dbb9f244bb20001df1f2",
+    "url": "/react-mablor/static/js/runtime-main.b8180a5d.js"
+  }
+]);
